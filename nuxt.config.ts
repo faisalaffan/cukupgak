d3ccee6429858c9a07d5a7118e7ce5b7704f6a7e
Muhammad Faisal Affan <faisallionel@gmail.com>
2026-05-07 15:47:32 +0700

    chore: add @nuxtjs/color-mode for dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
