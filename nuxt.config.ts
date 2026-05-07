7d3e6c5244974a34477153d9aea2217ca1cc4fa9
Muhammad Faisal Affan <faisallionel@gmail.com>
2026-05-07 15:01:52 +0700

    chore: init Nuxt 3 + TypeScript + Tailwind CSS

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  compatibilityDate: '2025-01-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+})
